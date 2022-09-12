7bbee6684e024310262e45a457a5a39e482fdca7
martin calero <calerom864@gmail.com>
2022-09-12 08:03:06 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'adulto_mayor',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
